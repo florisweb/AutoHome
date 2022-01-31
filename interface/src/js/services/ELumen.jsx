a8821ff4a4264088fb8a90d69a792e5170b917dc
florisweb <floris@florisweb.dev>
2022-01-31 14:10:54 +0100

    Fixed the online-indicator for the ELumen-panel.

--- interface/src/js/services/ELumen.jsx
+++ interface/src/js/services/ELumen.jsx
@@ -1,49 +1,51 @@
 
+{
+	const panel = new function() {
+		const This = this;
+		HomePagePanel.call(this, {
+			customClass: "ELumen",
+			onRender: render
+		});
+		
+		let renderOnlineIndicator = this.renderOnlineIndicator;
+		function render() {
+			let icon = <img className='panelIcon' src='images/eLumenIcon.png'></img>;
+			let state = <div className='text subText waterPercentage'>&#128167;52% filled</div>;
+			This.html["icon"] 		= icon;
 
-const ELumenPanel = new function() {
-	const This = this;
-	HomePagePanel.call(this, {
-		customClass: "ELumen",
-		onRender: render
-	});
-	
-	let renderOnlineIndicator = this.renderOnlineIndicator;
-	function render() {
-		let icon = <img className='panelIcon' src='images/eLumenIcon.png'></img>;
-		let state = <div className='text subText waterPercentage'>&#128167;52% filled</div>;
-		This.html["icon"] 		= icon;
+			let onlineIndicator = This.renderOnlineIndicator();
+			This.setOnlineState(This.service.state.deviceOnline);
 
-		let onlineIndicator = This.renderOnlineIndicator();
-		This.setOnlineState(This.service.state.deviceOnline);
-
-		return [
-			icon,
-			state,
-			<div className='text panelTitle'>{This.service.name}</div>,
-			onlineIndicator,
-		];
-	}
+			return [
+				icon,
+				state,
+				<div className='text panelTitle'>{This.service.name}</div>,
+				onlineIndicator,
+			];
+		}
 
 
-	this.updateData = function() {
-
-	}
-}
-
-const ELumen = new function() {
-	Service.call(this, {serviceId: 'ELumen', name: 'eLumen', homeScreenPanel: ELumenPanel});
-	this.state = {
-		humidty: 0,
-		temperature: 0,
-	};
-
-	this.onEvent = (_event) => {
-		switch (_event.type)
-		{
-			case "curState": 
-				this.state = _event.data;
-				this.homeScreenPanel.updateData();
-			break;
+		this.updateData = function() {
+			this.setOnlineState(this.service.state.deviceOnline);
 		}
 	}
+
+	const ELumen = new function() {
+		Service.call(this, {serviceId: 'ELumen', name: 'eLumen', homeScreenPanel: panel});
+		this.state = {
+			humidty: 0,
+			temperature: 0,
+		};
+
+		this.onEvent = (_event) => {
+			switch (_event.type)
+			{
+				case "curState": 
+					this.state = _event.data;
+					this.homeScreenPanel.updateData();
+				break;
+			}
+		}
+	}
+
 }
