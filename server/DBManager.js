5bbd82817c80a5363cedec2fd285eb76e646c0ed
florisweb <floris@florisweb.dev>
2024-11-07 20:07:42 +0100

    Made sure the DBManager only does one thing at a time to prevent corruption of files.

--- server/DBManager.js
+++ server/DBManager.js
@@ -15,12 +15,16 @@ const DBManager = new function() {
 }
 export default DBManager;
 
+
+let curRequestPromise;
 export function FileManager(_path) {
     const Path = _path;
     let ActualPath = dataStoragePath + '/' + Path;
 
     this.getContent = async function(_isJSON = true) {
-        return new Promise((resolve, error) => {
+        while (curRequestPromise) await curRequestPromise;
+
+        curRequestPromise = new Promise((resolve, error) => {
             fs.readFile(ActualPath, (err, content) => {
                 if (err) return error(err);
                 let parsedContent = content;
@@ -31,19 +35,40 @@ export function FileManager(_path) {
                 resolve(parsedContent);
             });
         });
+
+        return new Promise((resolve, error) => {
+            curRequestPromise.then((_res) => {
+                resolve(_res);
+                curRequestPromise = false;
+            }, (_error) => {
+                error(_error);
+                curRequestPromise = false;
+            });
+        });
     }
 
     this.fileExists = async () => {
-        return new Promise((resolve, error) => {
+        while (curRequestPromise) await curRequestPromise;
+        curRequestPromise = new Promise((resolve, error) => {
             fs.exists(ActualPath, (_result) => resolve(_result));
+        });
+        
+        return new Promise((resolve, error) => {
+            curRequestPromise.then((_res) => {
+                resolve(_res);
+                curRequestPromise = false;
+            }, (_error) => {
+                error(_error);
+                curRequestPromise = false;
+            });
         });
     }
 
     let writingPromise;
     this.writeContent = async function(_contentObj) {
-        while (writingPromise) await writingPromise;
+        while (curRequestPromise) await curRequestPromise;
 
-        writingPromise = new Promise((resolve, error) => {
+        curRequestPromise = new Promise((resolve, error) => {
             let string = JSON.stringify(_contentObj);
             fs.writeFile(ActualPath, string, (err) => {
                 writingPromise = false;
@@ -51,6 +76,15 @@ export function FileManager(_path) {
                 resolve(true);
             });
         });
-        return writingPromise;
+
+        return new Promise((resolve, error) => {
+            curRequestPromise.then((_res) => {
+                resolve(_res);
+                curRequestPromise = false;
+            }, (_error) => {
+                error(_error);
+                curRequestPromise = false;
+            });
+        });
     }
 }
